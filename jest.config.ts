613994d1a6dad5f863ccc836b5a3ab11fd6f8e6a
WillNewmarch <will.newmarch@brandwatch.com>
2022-03-04 12:35:59 +0000

    Test suite fixed

--- jest.config.ts
+++ jest.config.ts
@@ -2,13 +2,10 @@ import type { Config } from '@jest/types';
 
 const config: Config.InitialOptions = {
     verbose: true,
+    testEnvironment: 'jest-environment-node',
     transform: {
-        '^.+\\.tsx?$': 'ts-jest',
-    },
-    globals: {
-        'ts-jest': {
-            babelConfig: { presets: ['@babel/preset-env'] }
-        },
+        '^.+\\.jsx?$': [ 'babel-jest', { configFile: './babel.jest.js' } ],
+        '^.+\\.tsx?$': [ 'ts-jest', {} ]
     },
 };
 
